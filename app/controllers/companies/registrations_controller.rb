2725524f86b2073bfc200155d199bea1b2740a6b
samyoshon <samuelyshon@gmail.com>
2016-01-20 10:54:18 -0800

    logos working

--- app/controllers/companies/registrations_controller.rb
+++ app/controllers/companies/registrations_controller.rb
@@ -1,11 +1,13 @@
 class Companies::RegistrationsController < Devise::RegistrationsController
 
-private 
-	def update_resource(resource, params)
-    	resource.update_without_password(params)
-  	end
 
-  	def registration_params
-  		params.require(:company).permit(:email, :name, :description, :website, :facebook, :instagram, :twitter, :logo)
-	end
+	private
+
+		def update_resource(resource, params)
+			resource.update_without_password(params)
+		end
+
+	  	def registration_params
+	  		params.require(:company).permit(:email, :password, :password_confirmation, :name, :description, :website, :facebook, :instagram, :twitter, :logo)
+		end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -6,7 +6,8 @@ class Company < ActiveRecord::Base
 
   has_many :posts
 
-  has_attached_file :logo, :styles => { :large => "600x600>", :medium => "300x300>", :thumb => "100x100#" }
+  has_attached_file :logo, :styles => { :large => "600x600>", :medium => "300x300>", :small => "150x150#",
+  :thumb => "75x75#" }
 
   validates_attachment_content_type :logo, content_type: /\Aimage\/.*\Z/
 
