1d5aef64db64c595eb7be257a10234d72d034871
samyoshon <samuelyshon@gmail.com>
2016-01-20 11:47:05 -0800

    before adding click count

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -11,19 +11,24 @@ $(document).ready(function(){
 		$('.company-edit--section').hide();
 		$('#company-edit-' + editID).show();
 	});
-});
-//Added 'Page:Load' because Turbolinks screws up JS when using link_to
-$(document).on('page:load',function(){
-	$('#show-apply-now-btn').click(function(){
-		$('#show-apply-now-div').toggleClass("apply-now-active");
-		$('#show-apply-now-btn').hide();
-	});
 
-	$('.company-edit--options').click(function(){
-		$('.company-edit--options').removeClass("company-edit--options-active");
-		$(this).addClass("company-edit--options-active");
-		var editID = this.id;
-		$('.company-edit--section').hide();
-		$('#company-edit-' + editID).show();
-	});
+	$('.post-view-count').click(function(){
+		console.log(this.id);
+	})
 });
+
+// //Added 'Page:Load' because Turbolinks screws up JS when using link_to
+// $(document).on('page:load',function(){
+// 	$('#show-apply-now-btn').click(function(){
+// 		$('#show-apply-now-div').toggleClass("apply-now-active");
+// 		$('#show-apply-now-btn').hide();
+// 	});
+
+// 	$('.company-edit--options').click(function(){
+// 		$('.company-edit--options').removeClass("company-edit--options-active");
+// 		$(this).addClass("company-edit--options-active");
+// 		var editID = this.id;
+// 		$('.company-edit--section').hide();
+// 		$('#company-edit-' + editID).show();
+// 	});
+// });
