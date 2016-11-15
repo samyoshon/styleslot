6aabd984420061c83ec372afff2c2093beab2c07
samyoshon <samuelyshon@gmail.com>
2016-11-14 18:44:35 -0800

    search by country ID

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,22 @@ class PostsController < ApplicationController
 
     def index
         @search = Post.search(params[:q])
+
         @posts = @search.result.paginate(page: params[:page], per_page: 5).where(["created_at > ?", 30.days.ago]).order("created_at DESC")
 
-        @posts_uniq = ["Accounting/Finance", "Administrative", "Buyer", "Design", "Digital/eCommerce",  "Fit Model", "Human Resources", "IT", "Logistics/Supply Chain", "Marketing", "Merchandising", "Operations", "Pattern Making", "Photography", "Production", "Retail Store", "Sales", "Social Media", "Sourcing", "Stylist", "Other"]   
+        ### Find Countries That Have Job Posts - Used for filter option
+        @posts_all = Post.where(["created_at > ?", 30.days.ago]).order("created_at DESC")
+
+        @countries = []
+        @posts_all.each do |post|
+            @countries << post.state
+        end
+        @countries_uniq = @countries.uniq.sort_by!{|e| e.downcase}
+        
+        ### Related to Original Idea of Job Types
+        # @posts_uniq = ["Accounting/Finance", "Administrative", "Buyer", "Design", "Digital/eCommerce",  "Fit Model", "Human Resources", "IT", "Logistics/Supply Chain", "Marketing", "Merchandising", "Operations", "Pattern Making", "Photography", "Production", "Retail Store", "Sales", "Social Media", "Sourcing", "Stylist", "Other"]   
+
+
     end
 
     def new
