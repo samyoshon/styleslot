f35a8be5ae5e0c5aeff5958ba78989fb3815de6c
samyoshon <samuelyshon@gmail.com>
2016-11-26 17:49:53 -0800

    search by country resume works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,13 @@ class UsersController < ApplicationController
 	def index
         @q = User.ransack(params[:q])
 
-        @users = @q.result.paginate(page: params[:page], per_page: 30).where(["updated_at > ?", 30.days.ago]).order("updated_at DESC")
+        @users = @q.result.paginate(page: params[:page], per_page: 30).where(["updated_at > ?", 1.days.ago]).order("updated_at DESC")
 
         ## Find Countries That Have Job Posts - Used for filter option
-        @resumes = User.where(["updated_at > ?", 30.days.ago]).order("created_at DESC")
+        @resumes = User.where(["updated_at > ?", 1.days.ago]).order("created_at DESC")
 
         @top_countries = []
+
         @resumes.each do |resume|
             @top_countries << resume.countrya
             @top_countries << resume.countryb
