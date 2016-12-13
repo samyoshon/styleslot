19431bcba62f4a32d98b78c88eee0fad289c14e7
samyoshon <samuelyshon@gmail.com>
2016-12-12 18:23:46 -0800

    post.all

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,10 +9,14 @@ class PostsController < ApplicationController
     def index
         @search = Post.ransack(params[:q])
 
-        @posts = @search.result.paginate(page: params[:page], per_page: 30).where(["created_at > ?", 30.days.ago]).order("created_at DESC")
+        @posts = Post.all 
+
+        # @search.result.paginate(page: params[:page], per_page: 30).where(["created_at > ?", 30.days.ago]).order("created_at DESC")
 
         ### Find Countries That Have Job Posts - Used for filter option
-        @posts_all = Post.where(["created_at > ?", 30.days.ago]).order("created_at DESC")
+        @posts_all = Post.all
+
+        # @posts_all = Post.where(["created_at > ?", 30.days.ago]).order("created_at DESC")
 
         @countries = []
         @posts_all.each do |post|
