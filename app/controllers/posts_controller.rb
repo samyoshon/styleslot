4a57e00297f520b9fe447a25d04d142d688dc42f
samyoshon <samuelyshon@gmail.com>
2017-01-09 14:25:12 -0800

    sortby resumes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,9 +19,6 @@ class PostsController < ApplicationController
             @countries << post.country
         end
         @countries_uniq = @countries.uniq.sort_by!{|e| e.downcase}
-        
-        ### Related to Original Idea of Job Types
-        # @posts_uniq = ["Accounting/Finance", "Administrative", "Buyer", "Design", "Digital/eCommerce",  "Fit Model", "Human Resources", "IT", "Logistics/Supply Chain", "Marketing", "Merchandising", "Operations", "Pattern Making", "Photography", "Production", "Retail Store", "Sales", "Social Media", "Sourcing", "Stylist", "Other"]   
     end
 
     def new
--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -17,11 +17,7 @@ class ResumesController < ApplicationController
             @top_countries << resume.countryb
             @top_countries << resume.countryc
         end
-        @top_countries_uniq = @top_countries.uniq.sort!
-        # _by!{|e| e.downcase}
-        
-        ### Related to Original Idea of Job Types
-        # @posts_uniq = ["Accounting/Finance", "Administrative", "Buyer", "Design", "Digital/eCommerce",  "Fit Model", "Human Resources", "IT", "Logistics/Supply Chain", "Marketing", "Merchandising", "Operations", "Pattern Making", "Photography", "Production", "Retail Store", "Sales", "Social Media", "Sourcing", "Stylist", "Other"]   
+        @top_countries_uniq = @top_countries.uniq.sort_by!{|e| e.downcase}
     end
 
     def show
